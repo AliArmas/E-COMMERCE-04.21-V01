9c24dbd56667af4956c9e7c525764b9c4f991d01
Jose Ali Mendoza Armas <191287@ids.upchiapas.edu.mx>
2021-03-16 02:34:10 -0600

    add components

--- E-COMMERCE/src/app/app.module.ts
+++ E-COMMERCE/src/app/app.module.ts
@@ -6,13 +6,21 @@ import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
 import { ProductComponent } from './product/product.component';
 import { NvarComponent } from './nvar/nvar.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { FooterComponent } from './footer/footer.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UserComponent,
     ProductComponent,
-    NvarComponent
+    NvarComponent,
+    LoginComponent,
+    RegisterComponent,
+    FooterComponent,
+    LandingPageComponent
   ],
   imports: [
     BrowserModule,
