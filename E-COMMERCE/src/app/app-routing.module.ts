d145f70e08cbd50b10be9d14eab8bbedf7cb22e0
Jose Ali Mendoza Armas <191287@ids.upchiapas.edu.mx>
2021-03-16 03:19:36 -0600

    changes 1

--- E-COMMERCE/src/app/app-routing.module.ts
+++ E-COMMERCE/src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
+//librerias utilizadas
+
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { LoginComponent } from './login/login.component';
+import { ProductComponent } from './product/product.component';
+import { RegisterComponent } from './register/register.component';
+import { UserComponent } from './user/user.component';
+//--------------------------------------------------------------------------//
+//Declaracion de rutas en el proyecto
+//--------------------------------------------------------------------------//
+const routes: Routes = [
+  {path: '', redirectTo: 'home', pathMatch :'full'},
+  {path: 'home' ,component: LandingPageComponent},
+  {path: 'login', component: LoginComponent,/* canActivate:[LandingPageGuard],*/},
+  {path: 'register', component: RegisterComponent},
+  {path: 'products', component: ProductComponent},
+  {path: 'user', component : UserComponent},
+  //{path: 'carrito', component : CarritoComponent}
+];
+//---------------------------------------------------------------------------//
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
