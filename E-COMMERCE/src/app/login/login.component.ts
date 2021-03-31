790a7165b745d4e593860d0810ab8ddc5e17fe4e
Jose Ali Mendoza Armas <191287@ids.upchiapas.edu.mx>
2021-03-31 01:38:48 -0600

    log&reg

--- E-COMMERCE/src/app/login/login.component.ts
+++ E-COMMERCE/src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  resetEmail = new FormControl('');
+  loginForm!: FormGroup
+ 
+  constructor(private router: Router, private _formBuider : FormBuilder) { }
+    ngOnInit(): void {
+    this.loginForm = this._formBuider.group({
+      user: [''],
+      password:[''],
+    })  
   }
 
+  login(){
+    console.log(this.loginForm.value)
+  }
+
+  onRegisterGoogle(){
+    console.log("login con google")
+  }
+  
+  onRegisterFacebook(){
+    console.log("Registro en facebook")
+  }
+
+  resetPassword(){
+    console.log("")
+  }
+  
 }
--- E-COMMERCE/src/app/register/register.component.ts
+++ E-COMMERCE/src/app/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +9,62 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  // pattern : any = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
+  registerFrom = new FormGroup({
+    // ,Validators.pattern(this.pattern)]
+    user: new FormControl('', [Validators.required, Validators.minLength(5)]),
+    name: new FormControl('', [Validators.required, Validators.minLength(5)]),
+    email: new FormControl('', [Validators.required, Validators.minLength(5)]),
+    password: new FormControl('', [Validators.required, Validators.minLength(5)]),
+    status: new FormControl('', [Validators.required])
+  })
+  registerForm!: FormGroup;
+  constructor(private router: Router, private _formBuild: FormBuilder) { }
 
   ngOnInit(): void {
+    this.registerForm = this._formBuild.group({
+      user: ['', Validators.requiredTrue],
+      name: ['', Validators.requiredTrue],
+      email: ['', Validators.requiredTrue],
+      password: ['', Validators.requiredTrue],
+      status: ['', Validators.requiredTrue]
+    })
   }
 
+  // saveUsuario(): void {
+  //   const data = this.registerForm.value
+  //   this.svc.newUsuarioAPI(data).subscribe(response =>{
+  //     console.log(response);
+  //   },error => {
+  //     console.log(error);
+  //   });
+  // }--proxy-config proxy.json
+  registerPy() {
+    const data = this.registerForm.value;
+    //this.svc.registerPost(data.user,data.name,data.email,data.password,data.status)
+  }
+
+  onRegister() {
+    const { email, password } = this.registerFrom.value;
+    // this.svc.login(email,password);
+    console.log("Email: " + email)
+    console.log("Contraseña: " + password)
+    this.router.navigate(['/home'])
+  }
+
+  // create(){
+  //   const data = this.registerForm.value
+  //   this.svc.post(data).subscribe(response =>{
+  //       console.log(response);
+  //     },error => {
+  //     console.log(error);
+  //   });
+  // }
+  create(){
+    console.log("hola")
+  }
+
+  get() {
+    console.log("hola estamos imprimiendo desde register")
+  }
 }
