3aa917387a223c6cfb51d08c7a8b73938fafef1a
Zahid Yamil Ramos Velazquez <191341@ids.upchiapas.edu.mx>
2021-04-09 08:35:12 -0500

    changes

--- E-COMMERCE/src/app/quotation/quotation.component.ts
+++ E-COMMERCE/src/app/quotation/quotation.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import  jsPDF from 'jspdf';
+import html2canvas from 'html2canvas';
 
 @Component({
   selector: 'app-quotation',
@@ -12,4 +14,20 @@ export class QuotationComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  Gquotation(){
+    const doc = new jsPDF();
+    doc.text('Hello World!', 10,10)
+
+    // doc.fromHTML(document.getElementById(''), 10,10);
+    doc.save('Lista de equipos');
+  }
+  contenedor(){
+    const contenedor = document.querySelector('#contenedor');
+    contenedor?.classList.toggle('active');
+    // document.querySelector('#boton-menu').addEventListener('click',() =>{
+      
+    // });
+    
+  }
+  
 }
