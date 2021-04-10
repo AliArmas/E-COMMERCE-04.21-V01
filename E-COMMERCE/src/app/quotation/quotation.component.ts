6539063b9dbb4331f6609e1120e389cbeeee8e02
Zahid Yamil Ramos Velazquez <191341@ids.upchiapas.edu.mx>
2021-04-10 15:33:30 -0500

    pdf y cotizacion

--- E-COMMERCE/src/app/quotation/quotation.component.ts
+++ E-COMMERCE/src/app/quotation/quotation.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import  jsPDF from 'jspdf';
-import html2canvas from 'html2canvas';
+import { jsPDF } from 'jspdf';
+import domtoimage from 'dom-to-image';
+import { CanvasElement, NgCanvasElement, NgCanvas,CanvasComponent } from 'angular-canvas';
 
 @Component({
   selector: 'app-quotation',
@@ -14,19 +15,21 @@ export class QuotationComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  Gquotation(){
-    const doc = new jsPDF();
-    doc.text('Hello World!', 10,10)
+  pdf(){
+    var canvas = document.getElementById('pdf');
 
-    // doc.fromHTML(document.getElementById(''), 10,10);
-    doc.save('Lista de equipos');
+    domtoimage.toPng(canvas).then((dataUrl)=>{
+        let imagen= new Image();
+        imagen.src=dataUrl;/*obtengo el screenshot*/
+        let pdf = new jsPDF('l','mm','A4');/* creamos el pdf con jspdf, l es de landscape, mm: medidas en milímetros, y A4 el formato*/
+        pdf.addImage( imagen, 18, 10, 260,189); /*imagen: es la captura que insertaremos en el pdf, 18: margen izquierdo, 10: margen superior, 260:ancho, 189:alto, pueden jugar con estos valores, de esta forma me quedó prolijo en A4 horizontal*/
+        pdf.save( 'coti.pdf' ); /* descargamos el pdf con ese nombre.*/
+    }
+    );
   }
   contenedor(){
     const contenedor = document.querySelector('#contenedor');
     contenedor?.classList.toggle('active');
-    // document.querySelector('#boton-menu').addEventListener('click',() =>{
-      
-    // });
     
   }
   
